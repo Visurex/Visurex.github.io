541ea9f08f0983e75fe6af1202f7ad9460daa9dd
Lammens Andy <lammens.andy@hotmail.com>
2022-07-14 23:41:24 +0200

    master

---
+++ js/app.js
@@ -0,0 +1,34 @@
+/* 
+
+different data type: 
+undefined, null, boolean, string, symbol, bigint, number, and object.
+
+3 veriable type: first declare a veriable with var then assigne a veriable
+
+var 
+
+let 
+
+const 
+ corpus html
+
+*/
+var a;
+var b;
+var a  = 22;
+console.log(a)
+
+b=a;
+console.log(b)
+console.log(a)
+
+
+console.log(a,b)
+
+var Name = "Visurex";
+
+console.log(Name)
+console.log(a,b,Name)
+
+
+
